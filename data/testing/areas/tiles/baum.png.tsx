e6947d28769bff376f0e200e2a2920b7f484bbcf
Paul Merrill <pdm@pdm.me>
2014-12-24 10:00:06 -0800

    remove testing world data from this repo
    And move 'client.ini' into 'data/' directory.

--- data/testing/areas/tiles/baum.png.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="baum.png" tilewidth="16" tileheight="16">
- <image source="baum.png" width="128" height="128"/>
-</tileset>
--- data/testing/entities/player/ping.py
+++
@@ -1,1 +0,0 @@
-print "Info [Player] - location x:%d y:%d z:%.1f" % Entity.coords

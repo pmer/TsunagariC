ca28a9838bb11d79092b36246f6c7c39b00a55bc
Paul Merrill <pdm@pdm.me>
2016-12-27 16:11:34 -0800

    build-world: Update to make a packfile
    Used to make a ZIP file.

--- scripts/build-world.sh
+++ scripts/build-world.sh
@@ -3,5 +3,12 @@ here="$PWD/$(dirname $0)/.."
 root="$here/.."
 mkdir -p "$root/bin"
 cd "$root/data"
-echo $root/bin
-zip -r --symlinks -0 "$root/bin/testing.world" .
+
+packtool=$(find "$root" -name pack-tool | head -n 1)
+
+if [ ! -x "$packtool" ]; then
+	echo "Cannot find pack-tool"
+	exit 1
+fi
+
+"$packtool" create ../bin/testing.world *

685a75c7b4a2b97e3c8449954d14bcb7b9238199
Paul Merrill <pdm@pdm.me>
2017-12-30 16:46:43 -0800

    core/world: Minor code organization

--- src/core/world.cpp
+++ src/core/world.cpp
@@ -131,10 +131,11 @@ void World::draw() {
 
         // Zoom and pan the Area to fit on-screen.
         rvec2 padding = view.getLetterboxOffset();
+        rvec2 scale = view.getScale();
+        rvec2 scroll = view.getMapOffset();
+
         window.translate(-padding.x, -padding.y, [&] {
-            rvec2 scale = view.getScale();
             window.scale(scale.x, scale.y, [&] {
-                rvec2 scroll = view.getMapOffset();
                 window.translate(-scroll.x, -scroll.y, [&] {
                     area->draw();
                 });

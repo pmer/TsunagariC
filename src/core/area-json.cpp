100fd3418f97ee91d25a403cc696f4833e1371ae
Paul Merrill <pdm@pdm.me>
2019-06-24 05:47:04 -0700

    core/area-json: Fix bug in dirname()

--- src/core/area-json.cpp
+++ src/core/area-json.cpp
@@ -239,7 +239,7 @@ AreaJSON::processMapProperties(Unique<JSONObject> obj) noexcept {
 static StringView
 dirname(StringView path) noexcept {
     Optional<size_t> slash = path.rfind('/');
-    return !slash ? "" : path.substr(0, static_cast<size_t>(slash) + 1);
+    return !slash ? "" : path.substr(0, static_cast<size_t>(*slash) + 1);
 }
 
 bool
